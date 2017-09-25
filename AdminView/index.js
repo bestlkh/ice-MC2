98ad564990ad164ea5abfc06484f982a9ea920c7
bestlkh <bestlkh@gmail.com>
2017-09-24 22:47:11 -0400

    fix

--- AdminView/index.js
+++ AdminView/index.js
@@ -248,7 +248,10 @@ AdminView.prototype.setupApi = function () {
 
     this.app.get("/v1/api/admin/getTokens", checkAuth, function(req, res) {
         MongoClient.connect(constants.dbUrl, function (err, db) {
+            console.log(1, req.session.settings.chat.roomName);
             db.collection("tracking").find({roomName: req.session.settings.chat.roomName}, function (err, result) {
+                if (err) return res.status(500).json({status: 500, message: "Server error, could not resolve request"});
+                console.log(2, result);
                 return csv.stringify(result.students).pipe(res);
             }.bind(this));
         }.bind(this));

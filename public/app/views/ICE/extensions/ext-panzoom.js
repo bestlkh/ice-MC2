7f4447d0110b33b35c35823092d90b5d8703522a
ZiXian(Wannie) Chen <Wannie923@gmail.com>
2017-10-09 21:53:27 -0400

    pinch to zoom test

--- public/app/views/ICE/extensions/ext-panzoom.js
+++ public/app/views/ICE/extensions/ext-panzoom.js
@@ -11,6 +11,8 @@
 methodDraw.addExtension("panzoom", function () {
     canv = methodDraw.canvas;
     var $panzoom;
+    var zoom = 1;
+    var zoomInc = 0.25;
 
     return {
         svgicons: "extensions/zoom.svg",
@@ -21,35 +23,116 @@ methodDraw.addExtension("panzoom", function () {
             title: "Panzoom utility",
             icon: "extensions/zoom.svg",
             events: {
-                "mouseup": function () {
+                "click": function () {
                     canv.setMode("pan");
-                    $panzoom = $("#svgroot").panzoom({
-                        cursor: 'default',
-                        increment: 4,
-                        minScale: 0.5,
-                        maxScale: 16,
-                        rangeStep: 2,
-                        transition: true,
-                        duration: 200,
-                        easing: "ease-in-out",
-                        focal: {
-                            clientX: 108,
-                            clientY: 132
-                        }
+
+
+
+                    var workarea = $("#svgroot");
+
+                    if ($panzoom && $panzoom.panzoom("isDisabled")) {
+                        $panzoom.panzoom("enable");
+                    }
+                    else if (!$panzoom) {
+                        $panzoom = $("#svgroot").panzoom({
+
+                            increment: 4,
+                            minScale: 0.5,
+                            maxScale: 16,
+                            rangeStep: 2,
+                            transition: true,
+                            duration: 200,
+                            easing: "ease-in-out",
+                            focal: {
+                                clientX: 108,
+                                clientY: 132
+                            },
+                            disablePan: true,
+                            animate: true
+                        });
+                        workarea.bind("mouseup", function (e) {
+
+                            var mode = canv.getMode();
+
+                            if (mode === "pan") {
+                                if (e.shiftKey) zoom -= zoomInc;
+                                else zoom += zoomInc;
+                                if (zoom < 0.5) zoom = 0.5;
+                                else if (zoom > 16) zoom = 16;
+                                console.log("zoom: "+zoom);
+                                $panzoom.panzoom("zoom", zoom);
+                                workarea.attr({
+                                    width: 1920 * zoom * 2,
+                                    height: 1040 * zoom * 2
+                                });
+                                workarea.css({
+                                    'transform-origin': '0% 0% 0px'
+                                });
+                                $("#svgcanvas").css({
+                                    width: 1920 * zoom,
+                                    height: 1040 * zoom
+                                })
+                            }
+
+                        });
+
+                        var svg = $("#svgcanvas");
+
+                        svg.css({
+                            cursor: "zoom-in"
+                        });
+
+                        $(document).on('keydown keyup', function(e){
+
+                            var mode = canv.getMode();
+
+                            if (mode === "pan") {
+                                if (e.shiftKey) {
+
+                                    svg.css({
+                                        cursor: "zoom-out"
+                                    });
+
+                                }
+                                else {
+                                    svg.css({
+                                        cursor: "zoom-in"
+                                    });
+
+                                }
+                            }
+                        });
+                    }
+
+
+
+                    workarea.css({
+                        'transform-origin': '0% 0% 0px'
                     });
+
+
                 }
             }
         }],
         callback: function () {
-            $(document).bind("mouseup", function () {
+
+            $(document).bind("mouseup", function (e) {
+
                 setTimeout(function () {
                     var mode = canv.getMode();
                     if (mode !== "pan") {
-                        if ($panzoom) $panzoom.panzoom("destroy");
+                        if (!$panzoom) return;
+                        $panzoom.panzoom("disable");
+                        var svg = $("#svgcanvas");
+                        svg.css({
+                            cursor: "text"
+                        });
                     }
                 }, 100);
 
-            })
+            });
+
+
 
         },
         mouseDown: function (opts) {

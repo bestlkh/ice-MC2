f2613b931fd2c8163e7a96761016996e2a09e08b
ZiXian(Wannie) Chen <Wannie923@gmail.com>
2017-10-20 22:48:37 -0400

    more fixes

--- public/app/views/ICE/extensions/ext-panzoom.js
+++ public/app/views/ICE/extensions/ext-panzoom.js
@@ -49,42 +49,62 @@ methodDraw.addExtension("panzoom", function () {
 
 
                     canv.setMode("pan");
+                    if ($panzoom && $panzoom.panzoom("isDisabled")) {
+                        $panzoom.panzoom("enable");
+                        if (isMobile) return;
+                        var svg = $("#svgcanvas");
 
-                    var options = {
-                        preventDefault: true,
-                        transform_always_block: true,
-                        domEvents: true
-                    };
-                    var mc = new Hammer.Manager(workarea[0]);
-                    mc.add(new Hammer.Pan({ threshold: 0, pointers: 0 }));
-
-                    mc.add(new Hammer.Swipe()).recognizeWith(mc.get('pan'));
-                    mc.add(new Hammer.Rotate({ threshold: 0 })).recognizeWith(mc.get('pan'));
-                    mc.add(new Hammer.Pinch({ threshold: 0 })).recognizeWith([mc.get('pan'), mc.get('rotate')]);
-
-                    mc.add(new Hammer.Tap({ event: 'doubletap', taps: 2 }));
-                    mc.add(new Hammer.Tap());
-
-                    //mc.get('pinch').set({ enable: true });
-                    mc.on("pinchstart pinchmove", function (e) {
-                        console.log(e.scale);
-                        zoom = e.scale;
-                        if (zoom < 0.5) zoom = 0.5;
-                        else if (zoom > 16) zoom = 16;
-                        console.log(zoom);
-                        $panzoom.panzoom("zoom", zoom);
-                        workarea.attr({
-                            width: 1920 * zoom * 2,
-                            height: 1040 * zoom * 2
+                        svg.css({
+                            cursor: "zoom-in"
                         });
-                        workarea.css({
-                            'transform-origin': '0% 0% 0px'
+                    }
+                    else if (!$panzoom) {
+                        $panzoom = workarea.panzoom({
+
+
+                            transition: true,
+                            duration: 200,
+                            easing: "ease-in-out",
+                            disablePan: true,
+                            animate: true
                         });
-                        $("#svgcanvas").css({
-                            width: 1920 * zoom,
-                            height: 1040 * zoom
-                        })
-                    });
+
+                        var options = {
+                            preventDefault: true,
+                            transform_always_block: true,
+                            domEvents: true
+                        };
+                        var mc = new Hammer.Manager(workarea[0]);
+                        mc.add(new Hammer.Pan({threshold: 0, pointers: 0}));
+
+                        mc.add(new Hammer.Swipe()).recognizeWith(mc.get('pan'));
+                        mc.add(new Hammer.Rotate({threshold: 0})).recognizeWith(mc.get('pan'));
+                        mc.add(new Hammer.Pinch({threshold: 0})).recognizeWith([mc.get('pan'), mc.get('rotate')]);
+
+                        mc.add(new Hammer.Tap({event: 'doubletap', taps: 2}));
+                        mc.add(new Hammer.Tap());
+
+                        //mc.get('pinch').set({ enable: true });
+                        mc.on("pinchstart pinchmove", function (e) {
+                            console.log(e.scale);
+                            zoom = e.scale;
+                            if (zoom < 0.5) zoom = 0.5;
+                            else if (zoom > 16) zoom = 16;
+                            console.log(zoom);
+                            $panzoom.panzoom("zoom", zoom);
+                            workarea.attr({
+                                width: 1920 * zoom * 2,
+                                height: 1040 * zoom * 2
+                            });
+                            workarea.css({
+                                'transform-origin': '0% 0% 0px'
+                            });
+                            $("#svgcanvas").css({
+                                width: 1920 * zoom,
+                                height: 1040 * zoom
+                            })
+                        });
+                    }
                 },
                 "click": function () {
 

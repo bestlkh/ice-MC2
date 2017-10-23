aa2ad5e7f197965e55159e1c959fdda7f11471c0
Jun Zheng <junzheng@exchange.niyume.com>
2017-10-22 19:24:39 -0600

    Added event listeners & scripts

--- public/app/js/ui.js
+++ public/app/js/ui.js
@@ -1,4 +1,54 @@
+var chatTextboxFocused = false;
+var newChatBodyHeight = null;
+
+
 // Disable scrolling on mobile Webkit browsers
-onTouchMove(function(e){
-    e.preventDefault();
+// onTouchMove(function(e){
+//
+//     e.preventDefault();
+// });
+
+var virtualKeyboardHeight = function () {
+    var sx = document.body.scrollLeft, sy = document.body.scrollTop;
+    var naturalHeight = window.innerHeight;
+    window.scrollTo(sx, document.body.scrollHeight);
+    var keyboardHeight = naturalHeight - window.innerHeight;
+    window.scrollTo(sx, sy);
+    return keyboardHeight;
+};
+
+
+onChatTextboxFocus(function(e){
+    chatTextboxFocused = true;
+    setTimeout(function(){
+        newChatBodyHeight = window.innerHeight;
+        window.scrollTo(0, 0);
+    }, 300)
 });
+
+onChatTextboxKeyup(function(e){
+
+});
+
+onChatTextboxBlur(function(e){
+    chatTextboxFocused = false;
+});
+
+onMainLoop(function(e){
+    // $("#chatframe").css({
+    //     height: window.innerHeight
+    // });
+    // $("#chat_body_div").css({
+    //     height: $(window).height() - 500
+    // });
+    if(chatTextboxFocused){
+        $("#chat_body_div").css({
+            height: newChatBodyHeight - 100
+        })
+    } else {
+        $("#chat_body_div").css({
+            height: window.innerHeight - 100
+        })
+    }
+});
+

acd4cb6081caecc405a2b25546097556fb9faaab
Jun Zheng <junzheng@exchange.niyume.com>
2017-10-23 09:31:18 -0600

    Revert "Added event listeners & scripts"
    This reverts commit aa2ad5e7f197965e55159e1c959fdda7f11471c0.

--- public/app/js/events.js
+++ public/app/js/events.js
@@ -1,12 +1,6 @@
 var mainLoopHandlers = [];
 var touchMoveHandlers = [];
 
-var chatTextboxFocusHandlers = [];
-var chatTextboxBlurHandlers = [];
-var chatTextboxKeyupHandlers = [];
-
-var chatRoomLoadedHandlers = [];
-
 
 function onMainLoop(func){
     mainLoopHandlers.push(func);
@@ -16,54 +10,16 @@ function onTouchMove(func){
     touchMoveHandlers.push(func);
 }
 
-function onChatRoomLoaded(func){
-    chatRoomLoadedHandlers.push(func);
-}
-
-function onChatTextboxFocus(func){
-    chatTextboxFocusHandlers.push(func);
-}
-
-function onChatTextboxBlur(func){
-    chatTextboxBlurHandlers.push(func);
-}
-
-function onChatTextboxKeyup(func){
-    chatTextboxKeyupHandlers.push(func);
-}
-
-
 function fireEventChain(chain, e){
     for(i = 0; i < chain.length; i++){
         chain[i](e);
     }
 }
 
-$(function(){
-    // Main loop
-    setInterval(function(){
-        fireEventChain(mainLoopHandlers);
-    }, 100);
+setInterval(function(){
+    fireEventChain(mainLoopHandlers);
+}, 100);
 
-    // ontouchmove mobile event
-    document.ontouchmove = function(e){
-        fireEventChain(touchMoveHandlers, e);
-    };
-
-    $("#chat_body_div").ontouchmove = function(e) {
-        e.stopPropagation();
-    };
-
-    onChatRoomLoaded(function(){
-        // on chat textbox focus
-        $("#textArea").focus(function(e){
-            fireEventChain(chatTextboxFocusHandlers, e);
-        });
-        $("#textArea").blur(function(e){
-            fireEventChain(chatTextboxBlurHandlers, e);
-        });
-        $("#textarea").on('keyup', function(e){
-            fireEventChain(chatTextboxKeyupHandlers, e);
-        });
-    })
-});
+document.ontouchmove = function(e){
+    fireEventChain(touchMoveHandlers, e);
+};

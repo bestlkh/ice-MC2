0a5853c78009bef4b0bdb2afa2b1fa2e566b2be0
Jun Zheng <me@jackzh.com>
2017-11-24 16:00:52 -0800

    Added isEquation method for MessageText

--- public/app/js/chatroom/MessageText.js
+++ public/app/js/chatroom/MessageText.js
@@ -14,6 +14,8 @@ var MessageText = function(text){
 
     this.is_image = this.text.match(/^\[mc2-image\]/);
 
+    this.is_equation = this.text.match(/^\$\$.*\$\$$/);
+
     this.getRaw = function(){
         return this.text;
     };
@@ -34,8 +36,9 @@ var MessageText = function(text){
         return atob(this.attachments['svg-source']);
     };
 
-
-
+    this.isEquation = function(){
+        return this.is_equation;
+    };
 
     this.isImage = function(){
         return this.is_image;

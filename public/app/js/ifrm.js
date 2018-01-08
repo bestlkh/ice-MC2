536d605a5b469bfaf4924c4ad049e69c63b37737
Jun Zheng <me@jackzh.com>
2018-01-07 21:16:24 -0600

    Remove simulateFullScreen function

--- public/app/js/ifrm.js
+++ public/app/js/ifrm.js
@@ -53,23 +53,6 @@ function imageOpen(){
 	});
 }
 
-function simulateFullScreen(){
-	var doc = window.document;
-    var docEl = doc.documentElement;
-
-    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-	var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-	
-	if(!requestFullScreen)
-		return;
-    if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-      requestFullScreen.call(docEl);
-    }
-    else {
-      cancelFullScreen.call(doc);
-    }
-}
-
 function cancelFS(){
 	//alert("changing");
 	var doc = window.document;

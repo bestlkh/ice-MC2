fff37228c785405733eee5869630ef4ad609aa09
Jun Zheng <me@jackzh.com>
2018-01-07 21:20:05 -0600

    Deprecated unused functions

--- public/app/js/ifrm.js
+++ public/app/js/ifrm.js
@@ -12,37 +12,3 @@ function swapParentFrame(){
     var editor = parent.document.getElementById("editorframe");
     $(editor).removeClass("shown")
 }
-
-function swapTool(){
-	var SP = document.getElementById("tool_selectpath");
-	if(state == 0){
-		//alert(5);
-		state = 1;
-	}
-	else{
-		//alert(6);
-		state = 0;
-	}
-}
-
-function imageOpen(){
-	var chatScreen = document.getElementById("chatframe");
-	html2canvas(chatScreen).then(function(canvas) {
-	    // Export the canvas to its data URI representation
-	    var base64image = canvas.toDataURL("image/png");
-
-	    // Open the image in a new window
-	    window.open(base64image, "_blank");
-	});
-}
-
-function cancelFS(){
-	//alert("changing");
-	var doc = window.document;
-    var docEl = doc.documentElement;
-
-    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-
-    if(cancelFullScreen) cancelFullScreen.call(doc);
-}

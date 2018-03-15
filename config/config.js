c9276aa4fe6ec2c90ca80f03ddf88d0e321dada4
Jun Zheng <me@jackzh.com>
2018-03-15 04:34:19 -0400

    Created REST endpoints for image libraries

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    version: 'v1.0',
+    accreditedImageLibraries: [
+        {
+            'name': 'Default Library',
+            'auto-load': false,
+            'link': '/public/docs/img-libs/MC^2+Default+Image+Library+by+Jun+Zheng.json'
+        }
+    ]
+}

60cfb8789a2ca1e02b1e34f9d93852b49511978a
Jun Zheng <me@jackzh.com>
2018-03-27 19:28:41 -0400

    Change config

--- config/config.js
+++ config/config.js
@@ -5,6 +5,31 @@ module.exports = {
             'name': 'Default Library',
             'auto-load': true,
             'link': '/docs/img-libs/MC^2+Default+Image+Library+by+Jun+Zheng.json'
+        },
+        {
+            'name': 'Default Library',
+            'auto-load': true,
+            'link': '/docs/img-libs/Chemical Reaction Arrows by Jun Zheng.json'
+        },
+        {
+            'name': 'Default Library',
+            'auto-load': true,
+            'link': '/docs/img-libs/Math Trigonometric Functions by Jun Zheng.json'
+        },
+        {
+            'name': 'Default Library',
+            'auto-load': true,
+            'link': '/docs/img-libs/Physics Optics Library B by Jun Zheng.json'
+        },
+        {
+            'name': 'Default Library',
+            'auto-load': true,
+            'link': '/docs/img-libs/Physics Optics Library C by Jun Zheng.json'
+        },
+        {
+            'name': 'Default Library',
+            'auto-load': true,
+            'link': '/docs/img-libs/Physics Optics Library E by Jun Zheng.json'
         }
     ]
 }
